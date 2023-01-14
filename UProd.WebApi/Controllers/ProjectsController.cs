7235c91781b1eafb03d113851dc48c9f9cd089fc
d.shiriaev <d.shiriaev@bars.group>
2023-01-14 23:58:16 +0400

    **
    **

---
+++ UProd.WebApi/Controllers/ProjectsController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+
+namespace UProd.WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]/[action]")]
+public class ProjectsController : ControllerBase
+{
+    [HttpGet]
+    public List<object> List()
+    {
+        return new List<object>()
+        {
+            new
+            {
+                Name = "Тест",
+                Id = 1,
+                Description = "Тест"
+            },
+            new
+            {
+                Name = "Тест",
+                Id = 1,
+                Description = "Тест"
+            },
+            new
+            {
+                Name = "Тест",
+                Id = 1,
+                Description = "Тест"
+            }
+        };
+    }
+}
